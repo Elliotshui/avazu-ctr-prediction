714e5fa9cadd56eb9876b8e403d6fadcded25883
stx <shuitianxiao_eecs@pku.edu.cn>
2020-08-06 15:30:49 +0800

    add evaluation

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ import numpy as np
 import os
 from model.DeepModel import DeepModel
 from util.dataloader import construct_tf_dataset
+from tqdm import tqdm
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 
@@ -12,21 +13,24 @@ sess = tf.Session(graph=graph)
 
 # constructing tf dataset
 with graph.as_default():
-	train_dataset, field_size, feature_size = construct_tf_dataset('data/train_data', 'data/train_feature_dict')
-	train_iter = train_dataset.make_one_shot_iterator()
-	next_batch_op = train_iter.get_next()
+	train_set, valid_set, field_size, feature_size = construct_tf_dataset('data/train_data', 'data/train_feature_dict')
+	train_iter = train_set.make_one_shot_iterator()
+	valid_iter = valid_set.make_one_shot_iterator()
+	next_train_batch_op = train_iter.get_next()
+	next_valid_batch_op = valid_iter.get_next()
 
 # model
 args = {
 	'field_size': field_size,
 	'feature_size': feature_size,
-	'embedding_size': 8,
+	'embedding_size': 64,
 	'layers': [32, 32],
 	'learning_rate': 3e-4
 }
 model = DeepModel(args, graph, sess)
 model.init_weights()
 
+'''
 loss = 0.0
 for i in range(1, 10000):
 	next_batch = sess.run(next_batch_op)
@@ -37,3 +41,27 @@ for i in range(1, 10000):
 	if i % 1000 == 0:
 		print("batch ", i, " average loss: ", loss / 100)
 		loss = 0.0
+'''
+
+num_epoch = 5
+num_batch = int(600000 / 256)
+loss = 0.0
+
+# training
+for i in tqdm(range(1, num_epoch * num_batch + 1)):
+	next_batch = sess.run(next_train_batch_op)
+	features = next_batch['features']
+	label = next_batch['label']
+	loss += model.train_batch(features, label)
+	if i % num_batch == 0:
+		print("epoch ", i / num_batch, " average loss: ", loss / num_batch)
+
+# eval
+loss = 0.0
+eval_iter = 10000
+for i in tqdm(range(1, eval_iter + 1)):
+	next_batch = sess.run(next_eval_batch_op)
+	features = next_batch['features']
+	label = next_batch['label'] 
+	loss += model.eval(features, label)
+print("loss: ", loss / eval_iter)
--- util/dataloader.py
+++ util/dataloader.py
@@ -5,6 +5,7 @@ import pickle
 
 BATCH_SIZE = 256 
 SHUFFLE_BUFFER_SIZE = 512
+EPOCH = 5
 
 def construct_tf_dataset(data_path, feature_dict_path):
 	print("loading data...")
@@ -14,14 +15,23 @@ def construct_tf_dataset(data_path, feature_dict_path):
 
 	field_size = len(train_data['features'][0])
 	feature_size = len(feature_dict)
+	record_num = len(train_data['features'])
 	print("field_size: ", field_size)
 	print("feature_size: ", feature_size)
+	print("record_number: ", record_num)
+
+	train_size = int(0.8 * record_num)
 
 	print("constructing tf dataset...")
 	dataset = tf.data.Dataset.from_tensor_slices(train_data)
-	dataset = dataset.batch(BATCH_SIZE).shuffle(SHUFFLE_BUFFER_SIZE)
+	dataset = dataset.shuffle(SHUFFLE_BUFFER_SIZE)
+	train_set = dataset.take(train_size)
+	validation_set = dataset.skip(train_size)
+
+	train_set = train_set.batch(BATCH_SIZE).shuffle(SHUFFLE_BUFFER_SIZE).repeat(EPOCH)
+	validation_set = validation_set.batch(BATCH_SIZE)
 	print("======== tf dataset constructed ========")
 
 
-	return dataset, field_size, feature_size
+	return train_set, validation_set, field_size, feature_size 
 
